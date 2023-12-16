971b985cc3c2d530f834723b8744cb5a496ea7cc
Tebogo Rafapa <121289022+TebogoRaf@users.noreply.github.com>
2023-12-17 00:38:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a number you want to check:");
+            int EnteredNumber = int.Parse(Console.ReadLine());
+            
+            CheckPalindrome(EnteredNumber);
+           
+        }
+        
+            public static void CheckPalindrome(int number)
+            {
+                int InitialNumber = number;
+                int reversed = 0;
+
+                while (number > 0)
+                {
+                    int digit = number % 10;
+                    reversed = (reversed * 10) + digit;
+                    number /= 10;
+                }
+                if(InitialNumber == reversed)
+                {
+                    Console.WriteLine("{0} is a palindrome ", InitialNumber);
+                }
+                else
+                {
+            Console.WriteLine("{0} is not a palindrome, ", InitialNumber);
+                }
+            }
+        }
+    
